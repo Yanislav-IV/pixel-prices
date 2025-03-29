6c9575c5af75a6396f8415d30ce269bb8199af75
Yanislav Ivаnov <yanislav.tiv@gmail.com>
2025-03-29 20:55:50 +0200

    Update check-prices.py

--- check-prices.py
+++ check-prices.py
@@ -1,21 +1,12 @@
 import sys, time
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+import undetected_chromedriver as uc
+from bs4 import BeautifulSoup
 
 print("Hello World")
 sys.stdout.flush()
 
-options = Options()
-options.add_argument("--headless")
-options.add_argument("--disable-gpu")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-
-service = Service("/usr/bin/chromedriver")
-driver = webdriver.Chrome(service=service, options=options)
-url = "https://www.buybest.bg/manufacturers/google?category=1&per-page=24"
-driver.get(url)
+driver = uc.Chrome()
+driver.get("https://www.buybest.bg/manufacturers/google?category=1&per-page=24")
 time.sleep(10)
 html = driver.page_source
 print(html)
