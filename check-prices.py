ef3d1045444f52ca8ee4b3bf6a4dd7fc76cc8713
Yanislav Ivаnov <yanislav.tiv@gmail.com>
2025-03-29 20:38:47 +0200

    Update check-prices.py

--- check-prices.py
+++ check-prices.py
@@ -1,10 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0"}
+
 def extract_price(c):
-    ip = c.find('strong').get_text(strip=True)
-    dp = c.find('sup').get_text(strip=True)
-    return float(ip + "." + dp)
+    return float(c.find('strong').get_text(strip=True) + "." + c.find('sup').get_text(strip=True))
 
 def parse_phone(c):
     name = c.find('p', class_='item-brand').get_text(strip=True)
@@ -13,7 +13,7 @@ def parse_phone(c):
     return {"name": name, "link": link, "price": price}
 
 def get_all_phones(url):
-    soup = BeautifulSoup(requests.get(url).content, 'html.parser')
+    soup = BeautifulSoup(requests.get(url, headers=headers).content, 'html.parser')
     return [parse_phone(c) for c in soup.find_all('div', class_='mobile-width')]
 
 url = "https://www.buybest.bg/manufacturers/google?category=1&per-page=24"
