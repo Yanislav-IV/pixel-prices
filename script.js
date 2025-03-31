67eff6e2e730ba2ccbcaf476a85930dbb2ece99c
Yanislav-IV <yanislav.tiv@gmail.com>
2025-04-01 01:04:10 +0300

    fix label

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ Papa.parse("phone_prices.csv", {
       phoneColors[phone] = randomColor();
       const dataPoints = groups[phone].map(pt => ({ x: pt.date, y: pt.price }));
       datasets.push({
-	label: formatPhoneName( phone,
+	label: phone,
         data: dataPoints,
         borderColor: phoneColors[phone],
         backgroundColor: phoneColors[phone],
